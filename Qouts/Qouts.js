9f730347954ac103fb776fc5ea38ab56fe38198f
Mohammed Hussein <mohammed4shado@gmail.com>
2025-04-28 19:30:20 +0300

    Add files via upload

---
+++ Qouts/Qouts.js
@@ -0,0 +1,45 @@
+// alert("Wellcome in Our Page To Create a new Qouts");
+
+// var qout = [
+//     {
+//         quote: "The most important thing is not to stop questioning. Curiosity has its own reason for existing.",
+//         author: "Albert Camus"
+//     },
+//     {
+//         quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+//         author: "Charles Dickens"
+//     },
+//     {
+//         quote: "The secret to a happy life is to eat what you love and let it live.",
+//         author: "Gandalf the Grey"
+//     },
+//     {
+//         quote: "The only way to do great work is to love what you do.",
+//         author: "Haruki Murakami"
+//     },
+//     {
+//         quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+//         author: "Louis Armstrong"
+//     },
+//     {
+//         quote: "If you can't explain it simply, you don't understand it well enough.",
+//         author: "Marie Curie"
+//     },
+//     {
+//         quote: "It is our choices, Harry, that show what we truly are, far more than our abilities.",
+//         author: "Bob Dylan"
+//     },
+//     {
+//         quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+//         author: "Winston Churchill"
+//     }
+// ];
+
+// var displayQuotes = document.getElementById("quoteDisplay");
+
+// function showRandomQuotes() {
+//     var randomIndex = Math.floor(Math.random() * qout.length);
+//     var selectedQuote = qout[randomIndex];
+//     displayQuotes.innerText = selectedQuote.quote + " - " + selectedQuote.author;
+// }
+
